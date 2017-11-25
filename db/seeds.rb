d2e1e9a10f9b4270cae33b61f3fd79d2ee9c2b11
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-25 09:36:12 -0500

    restaurant routes done - not yet reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+5.times do |i|
+  puts "Restaurant #{i}..."
+  Restaurant.create!({
+    name:         Faker::Hipster.words(2).join(" "),
+    address:      "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:     ["chinese", "italian", "japanese", "french", "belgian"].sample(),
+  })
+end
+puts 'Finished!'
